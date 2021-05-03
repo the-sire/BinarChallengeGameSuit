dfa78b92bf7442bca8174986833d033436dbcaf1
Desire Ramadhani <ramadhani.desire@gmail.com>
2021-05-03 22:19:44 +0700

    Update Fix

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -21,7 +21,7 @@ class App {
         PeraturanSuit().aturanSuit()
     }
 
-    }
+}
 
 
 
--- src/main/kotlin/utility/PeraturanSuit.kt
+++ src/main/kotlin/utility/PeraturanSuit.kt
@@ -2,23 +2,23 @@ package utility
 
 class PeraturanSuit() {
     fun aturanSuit(): String {
-        println("1. Masukkan Pemain 1: ")
-        val pemainPertama = readLine().toString()
-        println("2.Masukkan Pemain 2: ")
-        val pemainKedua = readLine().toString()
+        println("1. Masukkan Pemain 1 (Gunting/Batu/Kertas): ")
+        val pemainPertama = readLine()?.toLowerCase()
+        println("2.Masukkan Pemain 2 (Gunting/Batu/Kertas): ")
+        val pemainKedua = readLine()?.toLowerCase()
         return (if (pemainPertama == pemainKedua) {
             println("Hasil: SERI!")
-        } else if (pemainPertama == "Batu" && pemainKedua == "Gunting") {
+        } else if (pemainPertama == "batu" && pemainKedua == "gunting") {
             println("Hasil: Pemain 1 MENANG!")
-        } else if (pemainPertama == "Batu" && pemainKedua == "Kertas") {
+        } else if (pemainPertama == "batu" && pemainKedua == "kertas") {
             println("Hasil: Pemain 2 MENANG!")
-        } else if (pemainPertama == "Kertas" && pemainKedua == "Gunting") {
+        } else if (pemainPertama == "kertas" && pemainKedua == "gunting") {
             println("Hasil: Pemain 2 MENANG!")
-        } else if (pemainPertama == "Kertas" && pemainKedua == "Batu") {
+        } else if (pemainPertama == "kertas" && pemainKedua == "batu") {
             println("Hasil: Pemain 1 MENANG!")
-        } else if (pemainPertama == "Gunting" && pemainKedua == "Batu") {
+        } else if (pemainPertama == "gunting" && pemainKedua == "batu") {
             println("Hasil: Pemain 2 MENANG!")
-        } else if (pemainPertama == "Gunting" && pemainKedua == "Kertas") {
+        } else if (pemainPertama == "gunting" && pemainKedua == "kertas") {
             println("Hasil: Pemain 1 MENANG!")
         } else {
             println("WRONG INPUT!")
